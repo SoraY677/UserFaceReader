f14fd51aa0d3b7a6a4940f8a8545a961b6c80c3c
SoraY677 <ys4080@icloud.com>
2020-09-20 14:44:11 +0900

    びしゅうせい

--- main.py
+++ main.py
@@ -12,50 +12,41 @@ LEARNED_MODEL_PATH = "./learned-models/"
 PREDICTOR = dlib.shape_predictor(
     LEARNED_MODEL_PATH + 'shape_predictor_68_face_landmarks.dat')
 
-# 顔の位置を検出　返却値は位置を表すリスト(x,y,w,h)
-
-
 def face_position(gray_img):
-  faces = CASCADE.detectMultiScale(gray_img, minSize=(100, 100))
-  return faces
-
-# ランドマーク検出
-
+    faces = CASCADE.detectMultiScale(gray_img, minSize=(100, 100))
+    return faces
 
 def facemark(gray_img):
-  faces_roi = face_position(gray_img)
-  landmarks = []
-
-  for face in faces_roi:
-    detector = dlib.get_frontal_face_detector()
-    rects = detector(gray_img, 1)
+    faces_roi = face_position(gray_img)
     landmarks = []
 
-    for rect in rects:
-      landmarks.append(
-          numpy.array([[p.x, p.y] for p in PREDICTOR(gray_img, rect).parts()]))
+    for face in faces_roi:
+        detector = dlib.get_frontal_face_detector()
+        rects = detector(gray_img, 1)
+        landmarks = []
 
-  return landmarks
+        for rect in rects:
+            landmarks.append(
+                numpy.array([[p.x, p.y] for p in PREDICTOR(gray_img, rect).parts()]))
+    print(landmarks[0])
+    print(len(landmarks[0]))
+    return landmarks
 
 
 def main():
-  img = cv2.imread("./img/input.jpg")  # 自分の画像に置き換え
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 処理を早くするためグレースケールに変換
-  landmarks = facemark(gray)  # ランドマーク検出
+    img = cv2.imread("./img/input.jpg")  # 自分の画像に置き換え
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 処理を早くするためグレースケールに変換
+    landmarks = facemark(gray)  # ランドマーク検出
 
-  # ランドマークの描画
-  for landmark in landmarks:
-    for points in landmark:
-      cv2.drawMarker(img, (points[0], points[1]), (21, 255, 12))
+    # ランドマークの描画
+    for landmark in landmarks:
+        for points in landmark:
+            cv2.drawMarker(img, (points[0], points[1]), (21, 255, 12))
 
-  # 表示
-  cv2.imshow("video frame", img)
-  cv2.waitKey(0)
-
-  # 保存
-  cv2.imwrite("./result/output.jpg", img)
-  cv2.destroyAllWindows()
+    # 保存
+    cv2.imwrite("./result/output.jpg", img)
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-  main()
+    main()
